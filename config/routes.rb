48aef8eedd7735c1450f92a1096863ec39c26060
Raghu Betina <raghu@firstdraft.com>
2018-03-12 19:24:53 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "venues#index"
   # Routes for the Neighborhood resource:
   # CREATE
