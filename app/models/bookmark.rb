0cfb2c1eb80a0996a5a4f70478887e83788e3091
Raghu Betina <raghu@firstdraft.com>
2018-03-12 19:24:39 +0000

    Direct association between dishes and bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,9 @@
 class Bookmark < ApplicationRecord
   # Direct associations
 
+  belongs_to :dish,
+             :counter_cache => true
+
   belongs_to :venue,
              :counter_cache => true
 
