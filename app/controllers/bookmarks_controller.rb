dbf7e78e9446b0c6182073e778de095d442d90d9
Raghu Betina <raghu@firstdraft.com>
2018-03-12 19:24:13 +0000

    Generate bookmarks resource with starter:resource

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,87 @@
+class BookmarksController < ApplicationController
+  def index
+    @bookmarks = Bookmark.all
+
+    render("bookmarks/index.html.erb")
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+
+    render("bookmarks/show.html.erb")
+  end
+
+  def new
+    @bookmark = Bookmark.new
+
+    render("bookmarks/new.html.erb")
+  end
+
+  def create
+    @bookmark = Bookmark.new
+
+    @bookmark.dish_id = params[:dish_id]
+    @bookmark.venue_id = params[:venue_id]
+    @bookmark.user_id = params[:user_id]
+    @bookmark.notes = params[:notes]
+    @bookmark.image = params[:image]
+
+    save_status = @bookmark.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/bookmarks/new", "/create_bookmark"
+        redirect_to("/bookmarks")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Bookmark created successfully.")
+      end
+    else
+      render("bookmarks/new.html.erb")
+    end
+  end
+
+  def edit
+    @bookmark = Bookmark.find(params[:id])
+
+    render("bookmarks/edit.html.erb")
+  end
+
+  def update
+    @bookmark = Bookmark.find(params[:id])
+
+    @bookmark.dish_id = params[:dish_id]
+    @bookmark.venue_id = params[:venue_id]
+    @bookmark.user_id = params[:user_id]
+    @bookmark.notes = params[:notes]
+    @bookmark.image = params[:image]
+
+    save_status = @bookmark.save
+
+    if save_status == true
+      referer = URI(request.referer).path
+
+      case referer
+      when "/bookmarks/#{@bookmark.id}/edit", "/update_bookmark"
+        redirect_to("/bookmarks/#{@bookmark.id}", :notice => "Bookmark updated successfully.")
+      else
+        redirect_back(:fallback_location => "/", :notice => "Bookmark updated successfully.")
+      end
+    else
+      render("bookmarks/edit.html.erb")
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+
+    @bookmark.destroy
+
+    if URI(request.referer).path == "/bookmarks/#{@bookmark.id}"
+      redirect_to("/", :notice => "Bookmark deleted.")
+    else
+      redirect_back(:fallback_location => "/", :notice => "Bookmark deleted.")
+    end
+  end
+end
---
+++ db/migrate/20180312192413_create_bookmarks.rb
@@ -0,0 +1,14 @@
+class CreateBookmarks < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.integer :dish_id
+      t.integer :venue_id
+      t.integer :user_id
+      t.text :notes
+      t.string :image
+
+      t.timestamps
+
+    end
+  end
+end
