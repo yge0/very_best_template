2605d46c196efbd150fbfb4dba773f2521289ba8
Raghu Betina <raghu@firstdraft.com>
2018-03-12 19:24:06 +0000

    Generate venues resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Venue resource:
+  # CREATE
+  get "/venues/new", :controller => "venues", :action => "new"
+  post "/create_venue", :controller => "venues", :action => "create"
+
+  # READ
+  get "/venues", :controller => "venues", :action => "index"
+  get "/venues/:id", :controller => "venues", :action => "show"
+
+  # UPDATE
+  get "/venues/:id/edit", :controller => "venues", :action => "edit"
+  post "/update_venue/:id", :controller => "venues", :action => "update"
+
+  # DELETE
+  get "/delete_venue/:id", :controller => "venues", :action => "destroy"
+  #------------------------------
+
   devise_for :users
   # Routes for the User resource:
   # READ
