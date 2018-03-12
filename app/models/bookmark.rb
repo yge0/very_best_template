4059a01f64df54751f3047129c260bbde75e7d74
Raghu Betina <raghu@firstdraft.com>
2018-03-12 19:24:23 +0000

    Validate presence of bookmarks.dish_id

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -5,6 +5,8 @@ class Bookmark < ApplicationRecord
 
   # Validations
 
+  validates :dish_id, :presence => true
+
   validates :user_id, :presence => true
 
   validates :venue_id, :uniqueness => { :scope => [:dish_id, :user_id], :message => "has already been bookmarked" }
